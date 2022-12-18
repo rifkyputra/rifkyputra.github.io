5c3360fdc7ad89d6e747cdd4010f9edad423d962
rifkyputra <rifqempul@gmail.com>
2022-12-18 13:21:16 +0700

    remove experimental

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    experimental: {
-      images: {
-        unoptimized: true,
-      },
+    images: {
+      unoptimized: true,
     },
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
