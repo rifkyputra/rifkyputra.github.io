1af92d148d55ff16551ea684a1a307f27729b96b
Rifky A. Putra <rifqempul@gmail.com>
2025-10-16 15:23:41 +0800

    site moved

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,91 +1,88 @@
-import Layout, { siteDescription, siteTitle } from '../components/layout'
-import Head from '../node_modules/next/head'
-import Link from '../node_modules/next/link'
-import utilStyles from '../styles/utils.module.css'
-import { getPostDataFromPath } from '../lib/posts'
+import Head from "next/head";
 
-import Skills from '../components/skills'
-import Experience from '../components/experience'
-import Projects from '../components/projects'
-import Education from '../components/projects'
-
-
-
-export default function Home({ skills, exp, proj, edu }) {
+export default function Home() {
   return (
-    <Layout home>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#f5f5f5",
+      }}
+    >
       <Head>
-        <title>{siteTitle}</title>
+        <title>Site Moved</title>
+        <meta name="description" content="Site has moved to pir.my.id" />
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>{siteDescription}</p>
-      </section>
-      <section className={`${utilStyles.flexContainer}`}>
-        <Link href={`https://github.com/rifkyputra`}>
-          <a><h2 className={` ${utilStyles.flexMenuItem}`}>Github</h2></a>
-        </Link>
-        <div className={`${utilStyles.dividerBtm}`}>&nbsp;</div>
-        <span className={`${utilStyles.dividerSpan} `}>||</span>
-        <Link href={`https://gitlab.com/rifkyputra`}>
-          <a><h2 className={` ${utilStyles.flexMenuItem}`}>Gitlab</h2></a>
-        </Link>
-        <div className={`${utilStyles.dividerBtm}`}>&nbsp;</div>
 
-        <span className={`${utilStyles.dividerSpan} `}>||</span>
-        <Link href={`https://www.linkedin.com/in/rptra/`}>
-          <a><h2 className={`${utilStyles.flexMenuItem}`}>LinkedIn</h2></a>
-        </Link>
-        <div className={`${utilStyles.dividerBtm}`}>&nbsp;</div>
+      <main style={{ textAlign: "center", padding: "2rem" }}>
+        <h1
+          style={{
+            fontSize: "2.5rem",
+            marginBottom: "1rem",
+            color: "#333",
+          }}
+        >
+          Site Moved
+        </h1>
 
+        <p
+          style={{
+            fontSize: "1.2rem",
+            marginBottom: "2rem",
+            color: "#666",
+          }}
+        >
+          This site has moved to:
+        </p>
 
-        <span className={`${utilStyles.dividerSpan} `}>||</span>
-        <Link href={`/blog`}>
-          <a><h2 className={`${utilStyles.flexMenuItem}`}>Blog</h2></a>
-        </Link>
-        <div className={`${utilStyles.dividerBtm}`}>&nbsp;</div>
+        <a
+          href="https://pir.my.id"
+          style={{
+            fontSize: "1.5rem",
+            color: "#0070f3",
+            textDecoration: "none",
+            border: "2px solid #0070f3",
+            padding: "1rem 2rem",
+            borderRadius: "8px",
+            display: "inline-block",
+            transition: "all 0.2s ease",
+          }}
+          onMouseOver={(e) => {
+            e.target.style.backgroundColor = "#0070f3";
+            e.target.style.color = "white";
+          }}
+          onMouseOut={(e) => {
+            e.target.style.backgroundColor = "transparent";
+            e.target.style.color = "#0070f3";
+          }}
+        >
+          https://pir.my.id
+        </a>
 
-        <span className={`${utilStyles.dividerSpan} `}>||</span>
-        <Link href={`https://twitter.com/rif_ptra`}>
-          <a><h2 className={`${utilStyles.flexMenuItem}`}>Twitter</h2></a>
-        </Link>
+        <p
+          style={{
+            fontSize: "1rem",
+            marginTop: "2rem",
+            color: "#888",
+          }}
+        >
+          You will be redirected automatically in a few seconds...
+        </p>
+      </main>
 
-
-
-      </section>
-
-      <section className={`${utilStyles.wrapMargin}`}>
-        <Experience postData={exp}></Experience>
-      </section>
-
-      <section className={`${utilStyles.wrapMargin}`}>
-        <Skills postData={skills}></Skills>
-      </section>
-
-      <section className={`${utilStyles.wrapMargin}`}>
-        <Projects postData={proj}></Projects>
-      </section>
-
-      <section className={`${utilStyles.wrapMargin}`}>
-        <Education postData={edu}></Education>
-      </section>
-
-    </Layout>
-  )
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          setTimeout(function() {
+            window.location.href = 'https://pir.my.id';
+          }, 3000);
+        `,
+        }}
+      />
+    </div>
+  );
 }
-
-export async function getStaticProps() {
-  const skills = await getPostDataFromPath('mdPages/skills.md')
-  const exp = await getPostDataFromPath('mdPages/experience.md')
-  const proj = await getPostDataFromPath('mdPages/projects.md')
-  const edu = await getPostDataFromPath('mdPages/education.md')
-  return {
-    props: {
-      skills,
-      exp,
-      proj,
-      edu,
-    }
-  }
-}
-
-
